b2ede83945576f38723510e53b8e5e51e23c6546
DeltaMokey <engincanozkan@outlook.com>
2024-01-24 18:25:02 +0300

    Using Enums for Texture Keys

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import TextureKeys from '../consts/TextureKeys';
 
 export default class Game extends Phaser.Scene
 {
@@ -7,27 +8,6 @@ export default class Game extends Phaser.Scene
     }
 
     create() {
-        this.anims.create({
-            key: 'rocket-mouse-run', // name of the animation
-            // helper to generate frames 
-            frames: this.anims.generateFrameNames('rocket-mouse', {
-                start: 1,
-                end: 4,
-                prefix: 'rocketmouse_run',
-                zeroPad: 2,
-                suffix: ''
-            }),
-            // manuel version of the helper to create frames
-            //frames: [
-            // { key: 'rocket-mouse', frame: 'rocketmouse_run01.png' },
-            // { key: 'rocket-mouse', frame: 'rocketmouse_run02.png' },
-            // { key: 'rocket-mouse', frame: 'rocketmouse_run03.png' },
-            // { key: 'rocket-mouse', frame: 'rocketmouse_run04.png' }
-            //],
-            frameRate: 10,
-            repeat: -1 // -1 to loop forever
-        });
-
         // store the width and height of the game screen
         const width: number = this.scale.width;
         const height: number = this.scale.height;
@@ -35,7 +15,7 @@ export default class Game extends Phaser.Scene
         //this.add.image(0, 0, 'background').setOrigin(0, 0);
         // change this.add.image to this.add.tileSprite
         // notice the changed parameters
-        this.add.tileSprite(0, 0, width, height, 'background').setOrigin(0);
+        this.add.tileSprite(0, 0, width, height, TextureKeys.Background).setOrigin(0);
         
         const middleOfWidht: number = width * 0.5; 
         const middleOfHeight: number = height * 0.5;
@@ -43,7 +23,7 @@ export default class Game extends Phaser.Scene
         this.add.sprite(
             middleOfWidht, 
             middleOfHeight,
-            'rocket-mouse',
+            TextureKeys.RocketMouse,
             'rocketmouse_fly01'
         ).play('rocket-mouse-run');
     }
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import TextureKeys from '../consts/TextureKeys';
 
 export default class Preloader extends Phaser.Scene 
 {
@@ -7,17 +8,38 @@ export default class Preloader extends Phaser.Scene
     }
 
     preload() {
-        this.load.image('background', 'assets/house/bg_repeat_340x640.png');
+        this.load.image(TextureKeys.Background, 'assets/house/bg_repeat_340x640.png');
     
         // load mouse character as atlas
         this.load.atlas(
-            'rocket-mouse',
+            TextureKeys.RocketMouse,
             'assets/characters/rocket-mouse.png',
             'assets/characters/rocket-mouse.json'
         );
     }
 
     create() {
+        this.anims.create({
+            key: 'rocket-mouse-run', // name of the animation
+            // helper to generate frames 
+            frames: this.anims.generateFrameNames(TextureKeys.RocketMouse, {
+                start: 1,
+                end: 4,
+                prefix: 'rocketmouse_run',
+                zeroPad: 2,
+                suffix: ''
+            }),
+            // manuel version of the helper to create frames
+            //frames: [
+            // { key: 'rocket-mouse', frame: 'rocketmouse_run01.png' },
+            // { key: 'rocket-mouse', frame: 'rocketmouse_run02.png' },
+            // { key: 'rocket-mouse', frame: 'rocketmouse_run03.png' },
+            // { key: 'rocket-mouse', frame: 'rocketmouse_run04.png' }
+            //],
+            frameRate: 10,
+            repeat: -1 // -1 to loop forever
+        });
+
         this.scene.start('game');
     }
 }
